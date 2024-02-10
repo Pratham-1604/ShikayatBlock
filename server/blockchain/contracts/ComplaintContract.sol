2b0ae561df822bbb1ccbcd3ac82c1a591f167868
Pratham-1604 <mundadaprathamesh@gmail.com>
2024-02-11 03:17:20 +0530

    updated contracts

--- server/blockchain/contracts/ComplaintContract.sol
+++ server/blockchain/contracts/ComplaintContract.sol
@@ -1,8 +1,5 @@
 pragma solidity ^0.8.0;
 
-// // Importing IPFS library
-// import "./IPFSStorage.sol";
-
 contract ComplaintContract {
     // Struct to represent a complaint
     struct ComplaintContract {
@@ -11,34 +8,52 @@ contract ComplaintContract {
         uint256 complaintId; // Complaint ID
         string subject; // Subject of the complaint
         string description; // Description of the complaint
+        string complaintType; // Description of the complaint
+        // string fileName;
         string ipfsHash; // IPFS hash of the complaint details
+        string date;
+        string status;
+        string statusType;
+        string authorityName;
+        uint256 priority;
     }
 
     // Mapping to store complaints
     mapping(uint256 => ComplaintContract) public complaints;
-
-    // Counter for generating complaint IDs
+    mapping(string => uint256[]) public complaintsByComplaintType;
+    mapping(string => uint256[]) public complaintsByAuthorityName;
     uint256 public complaintCount;
-
-    // Counter for generating complaint group ids
     uint256 public complaintGroupCount;
 
-    // Event to emit when a new complaint is submitted
     event NewComplaint(
         string userId,
         uint256 indexed complaintGroupId,
         uint256 indexed complaintId,
         string subject,
         string description,
-        string ipfsHash
+        string complaintType,
+        // string fileName,
+        string ipfsHash,
+        string date,
+        string status,
+        string statusType,
+        string authorityName,
+        uint256 priority
     );
 
     // Function to submit a new complaint
     function submitComplaint(
-        string memory  _userId,
+        string memory _userId,
         string memory _subject,
         string memory _description,
-        string memory _ipfs
+        string memory _complaintType,
+        // string memory _fileName,
+        string memory _ipfs,
+        string memory _status,
+        string memory _statusType,
+        string memory _authorityName,
+        string memory _date,
+        uint256 _priority
     ) public {
         // Incrementing complaint count
         complaintCount++;
@@ -51,17 +66,19 @@ contract ComplaintContract {
             complaintId: complaintCount,
             subject: _subject,
             description: _description,
-            ipfsHash: _ipfs
+            complaintType: _complaintType,
+            // fileName: _fileName,
+            ipfsHash: _ipfs,
+            status: _status,
+            date: _date,
+            statusType: _statusType,
+            authorityName: _authorityName,
+            priority: _priority
         });
 
-        // // Storing complaint details on IPFS and getting the hash
-        // string memory ipfsHash = ipfsStorage.storeData(abi.encode(newComplaint));
-
-        // Updating IPFS hash in the complaint object
-        // newComplaint.ipfsHash = ipfsHash;
-
-        // Storing the complaint object
         complaints[complaintCount] = newComplaint;
+        complaintsByComplaintType[_complaintType].push(complaintCount);
+        complaintsByAuthorityName[_authorityName].push(complaintCount);
 
         // Emitting event
         emit NewComplaint(
@@ -70,41 +87,69 @@ contract ComplaintContract {
             complaintCount,
             _subject,
             _description,
-            _ipfs
+            _complaintType,
+            // _fileName,
+            _ipfs,
+            _date,
+            _status,
+            _statusType,
+            _authorityName,
+            _priority
         );
     }
 
-    // Function to update an existing complaint
     function updateComplaint(
-        string memory userId,
+        string memory _userId,
         uint256 complaintGroupId,
         string memory _subject,
         string memory _description,
-        string memory _ipfs
+        string memory _complaintType,
+        // string memory _fileName,
+        string memory _ipfs,
+        string memory _status,
+        string memory _statusType,
+        string memory _authorityName,
+        string memory _date,
+        uint256 _priority
     ) public {
         // Incrementing complaint count for the new complaint object
         complaintCount++;
 
         // Creating a new complaint object for the updated information
         ComplaintContract memory newComplaint = ComplaintContract({
-            userId: userId,
+            userId: _userId,
             complaintGroupId: complaintGroupId,
             complaintId: complaintCount,
             subject: _subject,
             description: _description,
-            ipfsHash: _ipfs
+            complaintType: _complaintType,
+            // fileName: _fileName,
+            ipfsHash: _ipfs,
+            status: _status,
+            date: _date,
+            statusType: _statusType,
+            authorityName: _authorityName,
+            priority: _priority
         });
 
         complaints[complaintCount] = newComplaint;
+        complaintsByComplaintType[_complaintType].push(complaintCount);
+        complaintsByAuthorityName[_authorityName].push(complaintCount);
 
-        // Emitting event
         emit NewComplaint(
-            userId,
-            complaintGroupId,
+            _userId,
+            complaintGroupCount,
             complaintCount,
             _subject,
             _description,
-            _ipfs
+            _complaintType,
+            // _fileName,
+            _ipfs,
+            _date,
+            _status,
+            _statusType,
+            _authorityName,
+            _priority
         );
     }
 
@@ -114,7 +159,20 @@ contract ComplaintContract {
     )
         public
         view
-        returns (string memory, uint256, string memory, string memory, string memory)
+        returns (
+            string memory,
+            uint256,
+            string memory,
+            string memory,
+            string memory,
+            // string memory,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            uint256
+        )
     {
         ComplaintContract memory complaint = complaints[_complaintId];
         return (
@@ -122,12 +180,32 @@ contract ComplaintContract {
             complaint.complaintGroupId,
             complaint.subject,
             complaint.description,
-            complaint.ipfsHash
+            complaint.complaintType,
+            // complaint.fileName,
+            complaint.ipfsHash,
+            complaint.date,
+            complaint.status,
+            complaint.statusType,
+            complaint.authorityName,
+            complaint.priority
         );
     }
 
     // Function to retrieve latest complaint ID
     function getLatestComplaintId() public view returns (uint256) {
         return (complaintCount);
+    }
+
+    // Function to get complaints by complaint type
+    function getComplaintsByComplaintType(
+        string memory _complaintType
+    ) public view returns (uint256[] memory) {
+        return complaintsByComplaintType[_complaintType];
+    }
+
+    function getComplaintsByAuthorityName(
+        string memory _complaintType
+    ) public view returns (uint256[] memory) {
+        return complaintsByAuthorityName[_complaintType];
     }
 }
